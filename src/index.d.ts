466abaddbab13027d69ea1bcb97567aeb9e8023d
Qyokizzzz <1104597683@qq.com>
2022-08-25 01:00:14 +0800

    declare

---
+++ src/index.d.ts
@@ -0,0 +1,5 @@
+import { StickyProps } from './types';
+
+
+declare function StickyContainer(props: { [key: string]: unknown }): JSX.Element;
+declare function Sticky(props: StickyProps): JSX.Element;
